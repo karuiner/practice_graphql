2afa7da96cd0f21ce6810969230a42791966e94b
karuiner <intaekgong@gmail.com>
2022-08-19 22:36:13 +0900

    [Feat] grpahql schema 테스트 중

--- server/src/index.js
+++ server/src/index.js
@@ -5,10 +5,48 @@ const cors = require(`cors`);
 let schema = buildSchema(`
   type Query {
     hello: String
+    user:User
+    article:[Article]
+    subcomments:[Comment]
   }
+
+  type User {
+    _id: String
+    userName: String
+  }
+
+  type Article {
+    _id: String
+    writerId: String
+    content: String
+    comments:[Comment]
+
+  }
+
+  type Comment {
+    _id: String
+    writerId: String
+    comment: String
+    subcomments:[Comment]
+  }
+
 `);
 
-let root = { hello: () => "Hello world!" };
+let root = {
+  hello: async () => "Hello world!",
+  user: async () => "test",
+  article: async () => [
+    { _id: "1234", writeId: "2345", content: "test", comments: [] },
+  ],
+  subcomments: async () => [
+    {
+      _id: "23132",
+      writerId: "64754",
+      comment: "ttete",
+      subcomments: [],
+    },
+  ],
+};
 
 const app = express();
 
