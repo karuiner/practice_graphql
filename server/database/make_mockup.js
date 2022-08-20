a9e410be70fae1aef6e76acda45356d2e0b4b2fc
karuiner <intaekgong@gmail.com>
2022-08-20 16:22:25 +0900

    [Feat] 목업 데이터 생성 구현

--- server/database/make_mockup.js
+++ server/database/make_mockup.js
@@ -60,14 +60,72 @@ const Article = model("Article", articleSchema);
 const Comment = model("Comment", commentSchema);
 
 async function f() {
-  let user = await User.findOne();
-  if (user === null) {
-    user = await new User({
-      userName: "test",
+  let mode = "new";
+
+  // if (mode === "new") {
+  //   await User.collection.drop();
+  //   await Article.collection.drop();
+  //   await Comment.collection.drop();
+  // }
+
+  let editer = await User.findOne({ userName: "editer" });
+  if (editer === null) {
+    editer = new User({
+      userName: "editer",
     });
-    user = await user.save();
+    editer = await editer.save();
   }
-  console.log(user._id);
+  console.log(editer._id);
+
+  let article = await Article.findOne();
+
+  if (article === null) {
+    article = new Article({
+      writerId: editer._id,
+      contents: "test_content",
+    });
+    article = await article.save();
+  }
+
+  let users = await User.find({ userName: { $ne: "editer" } });
+  if (users === null || users.length === 0) {
+    users = [];
+    for (let i = 0; i < 50; i++) {
+      let userName = `userName${`${i}`.padStart(4, "0")}`;
+      users.push({ userName });
+    }
+    users = await User.insertMany(users);
+  }
+
+  let comments = await Comment.find(),
+    c = 0;
+  if (comments === null || comments.length === 0) {
+    for (let i = 0; i < 12; i++) {
+      let k = Math.floor(Math.random() * 50);
+      k = k > 49 ? 49 : k;
+      let l = Math.floor(Math.random() * 10);
+      l = l > 9 ? 9 : l;
+      console.log(k, users[k]);
+      let comment = new Comment({
+        writerId: users[k]._id,
+        articleId: article._id,
+        comment: `comment${`${c}`.padStart(4, "0")}`,
+      });
+      c++;
+      comment = await comment.save();
+      for (let j = 0; j <= l; j++) {
+        let subcomment = new Comment({
+          writerId: users[k]._id,
+          commentId: comment._id,
+          comment: `comment${`${c}`.padStart(4, "0")}`,
+        });
+        c++;
+        comment.subcomments.push(subcomment);
+        subcomment = await subcomment.save();
+      }
+      comment = await comment.save();
+    }
+  }
 
   await mongoose.disconnect();
 }
